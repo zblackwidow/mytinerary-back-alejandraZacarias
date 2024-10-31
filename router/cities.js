3655d2c01f07b1df4235f4294ae46c28afb78fc2
Black <alejandrazacariasrt@gmail.com>
2024-10-30 23:22:28 -0300

    agregando ruta de updateCities

--- router/cities.js
+++ router/cities.js
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import createCity from "../controllers/cities/create.js";
 import allCities from "../controllers/cities/read.js";
+import {updateCity} from "../controllers/cities/update.js";
 
 
 let router = Router();
 
 router.post("/create", createCity);
+
 router.get("/all", allCities);
+router.put("/update", updateCity);
 
 
 export default router;
