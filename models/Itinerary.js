21f7f84f2b07db6c8840fe1568f66cfcd4bb7137
Black <alejandrazacariasrt@gmail.com>
2024-10-31 22:37:33 -0300

    creado el modelo de Itinerary

---
+++ models/Itinerary.js
@@ -0,0 +1,19 @@
+import { Schema, model } from "mongoose"
+
+let collection = "itineraries"
+
+let schema = new Schema({
+    name: { type: String, required: true },
+    activities: { type: Boolean, required: true },
+    country: { type: String, required: true },
+    price: { type: Number, required: true },
+    Image: { type: String, required: true },
+    description: { type: String, required: true },
+    duration: { type: Number, required: true },
+    currency: { type: String, required: true },
+    hashtags: { type: String, required: true }
+})
+
+let Itinerary = model(collection, schema);
+
+export default Itinerary
